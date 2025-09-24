8f7ff2a59318b492e633eb315238608c4a6e6a1b
Vasuki t <iamvasukithangaraj@gmail.com>
2025-09-24 12:27:31 +0530

    added checkbox verification

--- script.js
+++ script.js
@@ -26,24 +26,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Testing user input
     if (!namePattern.test(fullName.value)) {
+        document.getElementById("nameError").style.color =
+        "red";
       document.getElementById("nameError").textContent =
         "Please provide a valid name.";
       validationSuccess = false;
     }
 
     if (!emailPattern.test(email.value)) {
+        document.getElementById("emailError").style.color =
+        "red";
       document.getElementById("emailError").textContent =
         "Please provide a valid email.";
       validationSuccess = false;
     }
 
     if (!passwordPattern.test(password.value)) {
+        document.getElementById("passwordError").style.color =
+        "red";
       document.getElementById("passwordError").textContent =
         "Password must be at least 8 characters.";
       validationSuccess = false;
     }
     // confirm password
     if (password.value === "" || confirmPassword.value === "") {
+         document.getElementById("confirmPasswordError").style.color =
+        "red";
       document.getElementById("confirmPasswordError").textContent =
         "Please enter both password fields.";
       validationSuccess = false;
@@ -57,6 +65,14 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("confirmPasswordError").style.color = "rgba(90, 248, 90, 0.7)";
       document.getElementById("confirmPasswordError").textContent =
         "Passwords match ✅";
+    }
+
+    // check box verification
+    if(!termsCheckbox.checked) {
+         document.getElementById("checkBoxError").style.color =
+        "red";
+        document.getElementById("checkBoxError").textContent="You must select checkBox before submitting"
+       
     }
   });
 });
